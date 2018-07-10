2fbabf2e71b1568b3e01853506fa7cff7ecbb03f
“wheatandcat” <wheatandcat@gmail.com>
2018-07-10 09:08:28 +0900

    README

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Query_1 = require("./Query");
-exports.Query = Query_1.default;
-var Mutation_1 = require("./Mutation");
-exports.Mutation = Mutation_1.default;
-var gql_1 = require("./gql");
-exports.gql = gql_1.default;
-//# sourceMappingURL=index.js.map
